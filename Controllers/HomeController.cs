599cd6c711b7ebfcd34b95d8ed2b032fdf516da3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-06-05 10:12:26 +0600

    first MVC application

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace HelloMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "I am a Programmer";
 
             return View();
         }
