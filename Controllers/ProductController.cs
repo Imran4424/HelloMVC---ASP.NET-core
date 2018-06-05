3bf438c721164f559c2949501bee5e5a47711cc5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-06-05 10:48:24 +0600

    first MVC application

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HelloMVC.Models;
+
+namespace HelloMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        
+    }
+}
