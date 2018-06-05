63b3e8f04724db68fd47680bd565e2415f6da862
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-06-05 10:56:17 +0600

    first MVC application

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -8,8 +8,15 @@ namespace HelloMVC.Controllers
     {
         public IActionResult ShowAll()
         {
-            
+            ViewData["Heading"] = "All Products";
 
+            var Products = new List<Product>();
+
+            Products.Add(new Product{ ID = 101, Name = "Apple", Price = 1.1});
+
+            Products.Add(new Product{ ID = 102, Name = "Bike", Price = 2.2});
+
+            Products.Add(new Product{ ID = 103, Name = "Calculator", Price = 3.3});
 
             return View(Products);
         }
